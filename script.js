7cbc481556e8c4208b96301a2aec2b9f532aee39
AKSHAY KAPOOR <akshaykapoor2204@gmail.com>
2022-05-09 10:23:45 +0530

    .

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 function update() {
-    var select = document.getElementById('choose');
-    var option = select.options[select.selectedIndex];
-    console.log(option);   
-    console.log(option.value);
-    select.addEventListener("click", () => {
-        document.body.style.backgroundColor="blue";
-        
-    })
-    var lol=lol.addEventListener("load",function() { changeBackground('red');
-            return lol; })
-
-
-
+    var sel1 = document.getElementById('choose').value;
+    document.body.style.backgroundColor = sel1;
 }
-update()
